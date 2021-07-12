104eb5dd76dbdc5697c65910fb96d490e4c8dfff
jaydonhu <jaydonhu@tencent.com>
2021-07-12 12:56:28 +0800

    feat: init

---
+++ game/game.js
@@ -0,0 +1,32 @@
+var data = [
+    {
+        "title": "2048",
+        "img": "./resource/2048.jpg",
+        "href": "./list/2048",
+        "text": "快来拼凑2048吧 "
+    },
+    {
+        "title": "狂奔的忍者",
+        "img": "./resource/renzhe.png",
+        "href": "./list/renzhe",
+        "text": "奔跑吧 ~ 上下上下 ~ 上上下下~ "
+    },
+    {
+        "title": "魔方",
+        "img": "./resource/mofang.png",
+        "href": "./list/mofang",
+        "text": "挑战一下？？？"
+    },
+    {
+        "title": "见缝插针",
+        "img": "./resource/zhen.png",
+        "href": "./list/zhen",
+        "text": "考验手速的时候到了！！！"
+    },
+    {
+        "title": "超级摩托车",
+        "img": "./resource/motuo.png",
+        "href": "./list/motuoche",
+        "text": "冲冲冲 ~ ~ ~ "
+    }
+]
