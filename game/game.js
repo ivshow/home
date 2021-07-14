7e609f5361676d6844c25d137981e7ead8203951
jaydonhu <jaydonhu@tencent.com>
2021-07-14 13:47:23 +0800

    feat: 增加飞机大战

--- game/game.js
+++ game/game.js
@@ -1,32 +1,38 @@
 var data = [
     {
+        "title": "飞机大战",
+        "img": "./resource/plane.jpeg",
+        "href": "./list/plane",
+        "text": "是时候展现真正的技术了!"
+    },
+    {
         "title": "2048",
         "img": "./resource/2048.jpg",
         "href": "./list/2048",
-        "text": "快来拼凑2048吧 "
+        "text": "快来拼凑2048吧"
     },
     {
         "title": "狂奔的忍者",
         "img": "./resource/renzhe.png",
-        "href": "./list/renzhe",
+        "href": "./list/ninja",
         "text": "奔跑吧 ~ 上下上下 ~ 上上下下~ "
     },
     {
         "title": "魔方",
         "img": "./resource/mofang.png",
-        "href": "./list/mofang",
+        "href": "./list/rubikcube",
         "text": "挑战一下？？？"
     },
     {
         "title": "见缝插针",
         "img": "./resource/zhen.png",
-        "href": "./list/zhen",
+        "href": "./list/needle",
         "text": "考验手速的时候到了！！！"
     },
     {
         "title": "超级摩托车",
         "img": "./resource/motuo.png",
-        "href": "./list/motuoche",
+        "href": "./list/motorbike",
         "text": "冲冲冲 ~ ~ ~ "
     }
 ]
